e8d8dc011661483b1caf843b1636a78118227e03
moslisnas <molicapacidad@gmail.com>
2015-04-01 20:11:39 +0200

    estructurando formulario

--- js/form.js
+++ js/form.js
@@ -1,4 +1,11 @@
 function showForm(){
 	var form = document.getElementById("form");
-	form.style.display = "inline";
+	if(form.style.display == "inline"){
+		form.style.display = "none";
+		form.style.margin = "0%";
+	}
+	else{
+		form.style.display = "inline";
+		form.style.margin = "5%";
+	}
 }
