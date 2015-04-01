59f8d99ed8ace006c1af66dc5bf95fcf157a5c5e
moslisnas <molicapacidad@gmail.com>
2015-04-01 21:38:18 +0200

    Terminado formulario.

--- js/form.js
+++ js/form.js
@@ -9,3 +9,52 @@ function showForm(){
 		form.style.margin = "5%";
 	}
 }
+
+function verificarFormulario(){
+	//Elementos HTML a consultar:
+	var user = document.getElementById("usuario");
+	var password = document.getElementById("contrasenia");
+	var name = document.getElementById("nombre");
+	var surnames = document.getElementById("apellidos");
+	var email = document.getElementById("correoElectronico");
+	//Elementos HTML a editar:
+	var errorDiv = document.getElementById("errorDiv");
+	var errorText = document.getElementById("errorMessage");
+
+	if(user.value.length < 3){
+		errorText.textContent = "El nombre de usuario no es suficientemente largo.";
+		errorDiv.style.display = "inline";
+	}
+	else if(password.value.length < 3){
+		errorText.textContent = "La contraseña no es suficientemente larga.";
+		errorDiv.style.display = "inline";
+	}
+	else if(name.value.length == 0){
+		errorText.textContent = "Es necesario que insertes tu nombre.";
+		errorDiv.style.display = "inline";
+	}
+	else if(apellidos.value.length == 0){
+		errorText.textContent = "Es necesario que insertes tus apellidos.";
+		errorDiv.style.display = "inline";
+	}
+	else if(!comprobarEmail(email)){
+		errorText.textContent = "La dirección de correo electrónico no es válida.";
+		errorDiv.style.display = "inline";
+	}
+	else{
+		errorDiv.style.display = "none";
+		alert("El registro se ha completado satisfactoriamente.");
+	}
+}
+
+function comprobarEmail(email){
+	var resultado = false;
+	var textEmail = email.value;
+	for (var i=0; i<textEmail.length; i++) {
+		if(textEmail[i] == "@"){
+			resultado = true;
+		}
+	}
+
+	return resultado;
+}
